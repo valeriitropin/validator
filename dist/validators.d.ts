5e1521047c0628495f7b0c6a29f785a465d9cb39
Valerii Tropin <tropin.valeriy@gmail.com>
2019-07-12 18:34:09 +0300

    - Update build

--- dist/validators.d.ts
+++ dist/validators.d.ts
@@ -5,7 +5,7 @@ export declare class Validators {
     static isString(field: string | number, value: any): Promise<string>;
     static isBoolean(field: string | number, value: any): Promise<boolean>;
     static isArray(field: string | number, value: any): Promise<any[]>;
-    static isObject(field: string, value: any): Promise<any>;
+    static isObject(field: string | number, value: any): Promise<any>;
     static minLength(min: number): (field: string, value: string) => Promise<string>;
     static maxLength(max: number): (field: string, value: string) => Promise<string>;
     static min(min: number): (field: string, value: number) => Promise<number>;
